014d844037d14343e3e5856e1a385b09a8be5965
Carley88 <carleyjeffrey88@gmail.com>
2020-08-24 16:40:10 +0100

    Created variable which holds the list of students and their details

--- js/script.js
+++ js/script.js
@@ -6,16 +6,7 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
+const studentList = document.querySelector(".student-list").children;
 
 function showPage (page, list) {
   const increment = (page - 1) * 10;
