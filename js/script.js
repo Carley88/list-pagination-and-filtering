5232e64a3e0932c71e04a4319fbe6790cb13353e
Carley88 <carleyjeffrey88@gmail.com>
2020-08-26 20:04:11 +0100

    Added notes to the js script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+
+/***
+studentList const stores the list containing the students details.
+paginationDiv const creates a new div with the class name pagination.
+***/
 const studentList = document.querySelector(".student-list").children;
 const paginationDiv = document.createElement("div");
 paginationDiv.className = 'pagination';
 
+/***
+showPage function:
+Calculates which 10 students will be displayed on the page according to which page number the user has clicked.
+@pageParameter is the page number the user has selected.
+@listParameter is the list containing the students details.
+***/
+
   function showPage (page, list) {
+    //The increment subtracts 1 from the selected page number to cater for the index begining at 0.
     const increment = (page - 1) * 10;
     const indexLow = 0 + increment;
     const indexHigh = 9 + increment;
@@ -15,6 +32,15 @@ paginationDiv.className = 'pagination';
       }
     }
   }
+
+/***
+appendPageLinks function:
+Calculates how many pages are required by dividing the length of the list by 10 & then adds each number to
+the bottom of the page.
+It then listens for a user clicking on the links, when clicked it calls the showPage function and passes the
+page number through the param.
+@listParameter is the list containing the students details.
+***/
 
   function appendPageLinks(list) {
     const pagesRequired = Math.ceil(list.length/10);
@@ -37,6 +63,15 @@ paginationDiv.className = 'pagination';
       showPage(page, list);
     })
   }
+
+/***
+search function
+Creates a search box at the top of the webpage so that a user can search for specific students.
+The search is not case sensitive and allows to find students by only typing 2 or 3 letters of their name.
+The results are returned to match the webpage design - only ten per page and the number of page links will
+be according to how many results are returned.
+If no results are returned a message will be displayed to let the user know & no page links are displayed.
+***/
 
 function search() {
   const studentNames = document.querySelectorAll("h3");
@@ -82,6 +117,11 @@ function search() {
     }
   });
 }
+
+/***
+Loop makes the first 10 students display when the user lands on the webpage.
+Calling the appendPageLinks and search functions makes the page links and search bar appear on the webpage.
+***/
 
 for (let i = 0; i < studentList.length; i++) {
     if (i > 9) {
